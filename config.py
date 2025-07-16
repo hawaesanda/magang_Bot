c87bc31d06bff4f8bb1fd52214fcf0554dbc6a32
awaaa <hawa.esanda@gmail.com>
2025-07-16 16:34:53 +0700

    refactor: comment out SECTION_COORDINATES for clarity

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ TARGET_CHAT_ID = "1003337187"
 TIMEZONE = pytz.timezone("Asia/Jakarta")
 
 # Daftar section dan posisi crop-nya (left, top, right, bottom)
-SECTION_COORDINATES = {
-    "FULL_DASHBOARD": (480, 80, 1700, 1020),  # keseluruhan canvas
-    "FULFILLMENT_FBB": (485, 80, 820, 370),   # baris 1 kolom 1
-    "ASSURANCE_FBB": (800, 170, 1210, 525),    # baris 1 kolom 2
-    "SCORE_CREDIT": (1200, 170, 1700, 330),    # baris 1 kolom 3
-    "FULFILLMENT_BGES": (485, 360, 820, 620), # baris 2 kolom 1
-    "ASSURANCE_BGES": (485, 620, 810, 990),  # baris 2 kolom 2
-    "MSA_ASSURANCE": (1205, 330, 1690, 690),  # baris 2 kolom 3
-    "MSA_CNOP": (800, 515, 1210, 998),        # baris 3 kolom 1
-    "MSA_QUALITY": (1205, 696, 1685, 995)    # baris 3 kolom 3
-}
-
+# SECTION_COORDINATES = {
+#     "FULL_DASHBOARD": (480, 80, 1700, 1020),  # keseluruhan canvas
+#     "FULFILLMENT_FBB": (485, 80, 820, 370),   # baris 1 kolom 1
+#     "ASSURANCE_FBB": (800, 170, 1210, 525),    # baris 1 kolom 2
+#     "SCORE_CREDIT": (1200, 170, 1700, 330),    # baris 1 kolom 3
+#     "FULFILLMENT_BGES": (485, 360, 820, 620), # baris 2 kolom 1
+#     "ASSURANCE_BGES": (485, 620, 810, 990),  # baris 2 kolom 2
+#     "MSA_ASSURANCE": (1205, 330, 1690, 690),  # baris 2 kolom 3
+#     "MSA_CNOP": (800, 515, 1210, 998),        # baris 3 kolom 1
+#     "MSA_QUALITY": (1205, 696, 1685, 995)    # baris 3 kolom 3
+# }
+CROP_MSAWSA = (480, 80, 1700, 1020)
 CROP_PILATEN = (480, 80, 1700, 1020)
